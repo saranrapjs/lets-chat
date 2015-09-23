8f12b60365552771987d950ced70cb5b3d1ed095
Jeff Sisson <jeff@bigboy.us>
2015-09-23 11:32:19 -0400

    use something in the circle after all

--- app/core/avatars/initials.js
+++ app/core/avatars/initials.js
@@ -20,7 +20,7 @@ AvatarProvider.prototype.fetch = function(user, query, cb) {
         'style="width: 50px; height: 50px; background-color: rgb(' + colors[0] + ', ' + colors[1] + ', ' + colors[2] + ');">' +
             '<text text-anchor="middle" y="50%" x="50%" dy="0.36em" pointer-events="auto" fill="#ffffff" ' +
             'font-family="HelveticaNeue-Light,Helvetica Neue Light,Helvetica Neue,Helvetica, Arial,Lucida Grande, sans-serif" ' +
-            'style="font-weight: 400; font-size: 22px;">  </text>' +
+            'style="font-weight: 400; font-size: 22px;">' + _.escape(user.displayName[0]).toUpperCase() + '</text>' +
         '</svg>';
 
     cb(null, {
